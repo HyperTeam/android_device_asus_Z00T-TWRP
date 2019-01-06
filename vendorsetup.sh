HyperTeam/android_device_asus_Z00L-TWRP

Z00L: Update Prebuilt Kernel and Flags
* Compiled from https://github.com/theimpulson/android_kernel_asus_msm8916 using Google's 4.9 Toolchain

vendorsetup.sh
@@ -1,1 +1,9 @@
-add_lunch_combo cm_Z00L-eng
+# This file is executed by build/envsetup.sh, and can use anything
+# defined in envsetup.sh.
+#
+# In particular, you can add lunch options with the add_lunch_combo
+# function: add_lunch_combo generic-eng
+
+for var in eng user userdebug; do
+  add_lunch_combo omni_Z00L-$var
+done

HyperTeam/android_device_asus_Z00T-TWRP

vendorsetup.sh
@@ -1,1 +1,9 @@
-add_lunch_combo cm_Z00T-eng
+# This file is executed by build/envsetup.sh, and can use anything
+# defined in envsetup.sh.
+#
+# In particular, you can add lunch options with the add_lunch_combo
+# function: add_lunch_combo generic-eng
+
+for var in eng user userdebug; do
+  add_lunch_combo omni_Z00T-$var
+done